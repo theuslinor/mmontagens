c240cb28ed616e1dd2f0f974fc73ac3a57952eef
unknown <matheusdossantos3157@gmail.com>
2026-03-19 16:07:13 -0300

    feat: carrossel de clientes e fix nas imagens

---
+++ src/data/clientes.js
@@ -0,0 +1,39 @@
+// src/data/clientes.js
+export const clientes = [
+    {
+        nome: 'Artcom Vidros',
+        logo: '/midia/clientes/artcom.png',
+        link: "https://artcomvidros.com.br/"
+    },
+    {
+        nome: 'Cheung Instalações',
+        logo: '/midia/clientes/cheung.png',
+        link: "https://www.cheunginstalacoes.com.br/"
+    },
+    {
+        nome: 'Mercadão do Lojista',
+        logo: '/midia/clientes/mercadao.png',
+        link: "https://www.mercadaodolojista.com.br/"
+    },
+    {
+        nome: 'Doural',
+        logo: '/midia/clientes/doural.png',
+        ilink: "https://www.doural.com.br/"
+    },
+    {
+        nome: 'Caldé Bijuterias',
+        logo: '/midia/clientes/calde.png',
+        link: "https://www.facebook.com/caldebijuterias/"
+    },
+    {
+        nome: 'Agosto Bijoux',
+        logo: '/midia/clientes/agosto.png',
+        link: "https://www.instagram.com/agosto_bijoux/"
+    },
+    {
+        nome: 'Vem que Tem Biju',
+        logo: '/midia/clientes/biju.png',
+        link: "https://www.instagram.com/vemquetembiju/"
+    },
+    // Para adicionar mais, basta copiar um bloco desses acima e colar aqui
+];
